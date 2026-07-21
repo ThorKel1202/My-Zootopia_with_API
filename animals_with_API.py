6a54e8c85a5011795fee0933146310aa5d508b57
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-21 16:13:51 +0200

    Make it nicer and pep8

--- animals_with_API.py
+++ animals_with_API.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 
 
 def get_animals(name):
+    """
+        Function sends request (with API_KEY in header) to API with the variable "name". "Name" is the user input
+        for the desired animal. It returns the json answer from the API or None if request is invalid.
+    """
+    
     api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
     response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
@@ -17,6 +22,14 @@ def get_animals(name):
 
 
 def serialize_animal(animal):
+    """
+       Serializes an animal into a string by getting the wanted information. here it is
+       the name of the animal, the FIRST location and out of its characteristics the diet
+       and the type. Each one is checked, if there is the asked data or not. Along with the
+       needed html-tags it becomes a complete html string for one animal out of the JSON file,
+       which will be returned as single_animal_output.
+    """
+    
     single_animal_output = ''
     single_animal_output += '<li class="cards__item">\n'
 
@@ -53,26 +66,43 @@ def serialize_animal(animal):
 
 
 def make_string(user_animal_json):
+    """
+        The function first put all the JSON data into "animals_data. Then it creates a string, called
+        "output". Then it iterates through all the animal data in the JSON file and sends each single
+        one to "serialize_animal". After the return, it appends all to "output" and creates a complete
+        string which can be copied directly into the html template for usage there. Finally, it returns
+        the complete "output" string to the main function to print it.
+    """
+    
     animals_data = json.loads(user_animal_json)
     output = ''
     for animal in animals_data:
         output += serialize_animal(animal)
     return output
 
-
-# 👉 ROUTE (Webseite)
 @app.route("/", methods=["GET", "POST"])
 def home():
     animals_html = ""
+    error_message = ""
 
     if request.method == "POST":
         animal_name = request.form.get("animal")
-
         json_data = get_animals(animal_name)
         if json_data:
-            animals_html = make_string(json_data)
+            animals_data = json.loads(json_data)
+            # Prüfung, ob Animal existiert!
+            if len(animals_data) == 0:
+                error_message = f"Sorry, the animal „{animal_name}“ does not exist."
+            else:
+                animals_html = make_string(json_data)
+        else:
+            error_message = "API error. Please try again."
 
-    return render_template("index.html", animals=animals_html)
+    return render_template(
+        "animals_with_api.html",
+        animals=animals_html,
+        error=error_message
+    )
 
 
 if __name__ == "__main__":
